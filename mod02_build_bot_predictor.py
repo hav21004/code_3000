25d8f7c18798fcb2dc1dc35d93794cfd06613d46
Hayden Verney <hayden.verney@uconn.edu>
2026-02-10 11:02:10 -0500

    hw2 better

--- mod02_build_bot_predictor.py
+++ mod02_build_bot_predictor.py
@@ -10,11 +10,13 @@ def train_model(X, y, seed=seed):
     Build a GBM on given data
     """
     model = GradientBoostingClassifier(
-        learning_rate=0.05,
-        n_estimators=500,
-        max_depth=4,
+        learning_rate=0.06,
+        n_estimators=250,
+        max_depth=3,
         subsample=0.8,
         min_samples_leaf=6,
+        min_samples_split=12,
+        max_features=0.8,
         random_state=seed
     )
     model.fit(X, y)
